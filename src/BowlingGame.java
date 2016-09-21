0bcce1aeec71db60ceccfbbad34bf75fa72d14d2
somename <someemail>
2016-09-21 15:09:49 +0300

    besouro automatic message

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -6,20 +6,43 @@ import java.util.List;
 
 public class BowlingGame {
 	//a bowling game is made of (at least) 10 frames
+	private List<Frame> game = new ArrayList<Frame>();
 	private List<Frame> frames = new ArrayList<Frame>();
 	private Frame bonus;
 	
 	public BowlingGame(){
+		int[] fGames = { 1,1,1,1,1,1,1,1,1,1 };
+		int[] sGames = { 1,1,1,1,1,1,1,1,1,1 };
+		
 		try {
 			Frame aFrame = new Frame(1,1);
 		} catch (BowlingException e) {
 			System.err.println("Trying to create invalid frame: " + e.getMessage());
+		}
+
+		try {
+			createGame(fGames, sGames, game);
+		} catch (BowlingException e) {
+			System.err.println("Trying to create invalid game: " + e.getMessage());
+		}
+	}
+	
+	public void createGame(int first[], int second[], List<Frame> aGame) throws BowlingException {
+		if (first.length != second.length)
+			throw new BowlingException("Throw amounts do not match");
+		
+		if(!(first.length >= 10 && second.length >= 10))
+			throw new BowlingException("Can't create enough frames for game");
+		
+		for (int i = 0; i < first.length; i++) {
+			addFrame(new Frame(first[i], second[i]));
 		}
 	}
 	
 	// adds a frame to the game
 	public void addFrame(Frame frame){
 		//to be implemented
+		game.add(frame);
 	}
 	
 	// Sets the bonus throws at the end of the game
